b2fddb4f4605b97883d4769ddc1522ed6f032012
GSrman <99390711+GSrman@users.noreply.github.com>
2022-03-29 20:15:18 +0200

    Add files via upload

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# The s1234567 has to be replaced by your own log in codes.
+# Password confirmation will be asked twice.
+#
 mkdir resmet_countfailliet
 cd resmet_countfailliet
+# Get April 2020 tweets
 mkdir 2020
 scp -r s1234567@karora.let.rug.nl:/net/corpora/twitter2/Tweets/2020/04/ 2020
+# Get April 2019 tweets
 mkdir 2019
 scp -r s1234567@karora.let.rug.nl:/net/corpora/twitter2/Tweets/2019/04/ 2019
